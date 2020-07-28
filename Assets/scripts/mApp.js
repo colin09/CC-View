c3ac2be430b30da1ea48f428a1acdba887c0bb11
duxl <duxinglong@yijiupi.com>
2020-07-29 01:52:27 +0800

    + assets

---
+++ Assets/scripts/mApp.js
@@ -0,0 +1,13 @@
+﻿var mApp = angular.module('mApp', ['ngRoute', 'ngSanitize']);
+
+
+module.config(function ($routeProvider) {
+    $routeProvider.when('/index', //数据摘要
+        {
+            templateUrl: '/M/Home/Index',
+            controller: "indexController"
+        })
+        .otherwise({
+            redirectTo: "/dataDigest"
+        });
+});
